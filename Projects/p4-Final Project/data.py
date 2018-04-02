d324e60149b6e85d200086378c0e6cec6155a1cf
EMeireles <emimeirl@umich.edu>
2018-04-02 16:51:06 -0400

    Added function descriptions

--- Projects/p4-Final Project/data.py
+++ Projects/p4-Final Project/data.py
@@ -1,3 +1,22 @@
 import tweepy
 import json
 
+#uses Youtube APIs to gather channel information 
+def get_channel_info()
+
+#gets current subscriber count of channel
+def get_current_subcribers()
+
+#scrapes Scoial Blade for subs gained over a period of time
+def get_timed_subs()
+
+#gets revenue over a period of time
+def get_timed_rev()
+
+#gets tweets and gives sentiment analysis 
+def get_tweet_senti()
+
+#gets tweets for youtuber mentioned
+def get_tweets()
+
+
