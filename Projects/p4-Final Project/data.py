36fcae0ead14747ceb1f1a862885db43dadb5d91
EC2 Default User <ec2-user@ip-172-31-42-146.us-east-2.compute.internal>
2018-04-09 02:29:24 +0000

    Adding URL requests

--- Projects/p4-Final Project/data.py
+++ Projects/p4-Final Project/data.py
@@ -1,23 +1,33 @@
 import tweepy
 import json
 import dstk
+import requests
 
 #uses Youtube APIs to gather channel information 
-def get_channel_info()
+def get_channel_info():
+    base_url='https://www.googleapis.com/youtube/v3/search'
+    data=requests.get(base_url,params)
+    
+    
 
 #gets current subscriber count of channel
-def get_current_subcribers()
+def get_current_subcribers():
+    pass
 
 #scrapes Scoial Blade for subs gained over a period of time
-def get_timed_subs()
+def get_timed_subs():
+    pass
 
 #gets revenue over a period of time
-def get_timed_rev()
+def get_timed_rev():
+    pass
 
 #gets tweets and gives sentiment analysis 
-def get_tweet_senti()
+def get_tweet_senti():
+    pass
 
 #gets tweets for youtuber mentioned
-def get_tweets()
+def get_tweets():
+    pass
 
 
