bd9578313043e147f5b8ac2c073570e8b3fb6367
EMeireles <emimeirl@umich.edu>
2018-04-11 13:49:27 -0400

    Added database code (may/may not keep) and caching

--- Projects/p4-Final Project/data.py
+++ Projects/p4-Final Project/data.py
@@ -1,8 +1,11 @@
 import secrets
 import json
+import tweepy
+import sqlite3
 import requests
 from bs4 import BeautifulSoup
 CACHE_FNAME="Youtube_Cache.json"
+db_name='YoutubeTweets.db'
 
 try:
     cache_file = open(CACHE_FNAME, 'r')
@@ -12,13 +15,6 @@ try:
 except:
     CACHE_DICTION = {}
 
-#class YoutubeChannel(object):
-#    """docstring for YoutubeChannel"""
-#    def __init__(self, arg):
-#        super(YoutubeChannel, self).__init__()
-#        self.arg = arg
-        
-
 
 def params_unique_combination(baseurl, params):
     alphabetized_keys = sorted(params.keys())
@@ -27,22 +23,26 @@ def params_unique_combination(baseurl, params):
         res.append("{}-{}".format(k, params[k]))
     return baseurl + "_".join(res)
 
-def make_request_using_cache(baseurl, params):
-    unique_ident = params_unique_combination(baseurl,params)
+def make_request_using_cache(baseurl, params=''):
+    if params!='':
+        unique_ident = params_unique_combination(baseurl,params)
+    else:
+        unique_ident=baseurl
 
-    ## first, look in the cache to see if we already have this data
     if unique_ident in CACHE_DICTION:
         print("Getting cached data...")
         return CACHE_DICTION[unique_ident]
 
-    ## if not, fetch the data afresh, add it to the cache,
-    ## then write the cache to file
     else:
         print("Making a request for new data...")
         # Make the request and cache the new data
-        resp = requests.get(baseurl, params)
-        CACHE_DICTION[unique_ident] = json.loads(resp.text)
-        dumped_json_cache = json.dumps(CACHE_DICTION)
+        if params !='':
+            resp = requests.get(baseurl, params)
+            CACHE_DICTION[unique_ident] = json.loads(resp.text)
+        else:
+            resp=requests.get(baseurl)
+            CACHE_DICTION[unique_ident] = resp.text
+        dumped_json_cache = json.dumps(CACHE_DICTION,indent=4)
         fw = open(CACHE_FNAME,"w")
         fw.write(dumped_json_cache)
         fw.close() # Close the open file
@@ -55,37 +55,36 @@ YoutubeAPI=secrets.YOUTUBE_API_KEY
 def get_channel_info(query):
     base_url='https://www.googleapis.com/youtube/v3/search'
     params={'part':'snippet','q':query,'type':'channel','key':YoutubeAPI}
-    tube_data=requests.get(base_url,params)
-    data=json.loads(tube_data.text)
-    return data
+    tube_data=make_request_using_cache(base_url,params)
+    return tube_data
 #gets current subscriber count of channel
 def get_current_subcribers(id_):
     base_url='https://www.googleapis.com/youtube/v3/channels'
     params={'id':id_,'part':'statistics','key':YoutubeAPI}
-    tube_data=requests.get(base_url,params)
-    data=json.loads(tube_data.text)
-    print(json.dumps(data,indent=2))
+    tube_data=make_request_using_cache(base_url,params)
+    return tube_data
 
-#scrapes Scoial Blade for subs gained over a period of time
+#scrapes Scoial Blade for all the data
 def get_social(channel):
     baseurl='https://socialblade.com/youtube/search/{}'.format(channel)
     dig_url='https://socialblade.com'
-    data=requests.get(baseurl)
-    soup=BeautifulSoup(data.text,'html.parser')
+    data=make_request_using_cache(baseurl)
+    soup=BeautifulSoup(data,'html.parser')
     boxes=soup.find_all('div', style='width: 1200px; height: 88px; background: #fff; padding: 15px 30px; margin: 2px auto; border-bottom: 2px solid #e4e4e4;')
     link=list(boxes[0].find('h2').children)[0]['href']
     #Gets Sumamry Data of of Social Balde
-    new_page=requests.get(dig_url+link)
-    page_soup=BeautifulSoup(new_page.text,'html.parser')
+    new_page=make_request_using_cache(dig_url+link)
+    page_soup=BeautifulSoup(new_page,'html.parser')
     summary=page_soup.find('div',style='width: 1200px; height: 250px; padding: 30px;')
     summary_list=[]
     for item in summary.find_all('p'):
         summary_list.append(item.text.strip())
+
     #Scrapes for the subscriber data and revenue data
     detailed_url=page_soup.find('div',id='YouTubeUserMenu')
     nav_list=detailed_url.find_all('a')
-    detailed_soup=requests.get(dig_url+nav_list[2]['href'])
-    sub_soup=BeautifulSoup(detailed_soup.text,'html.parser')
+    detailed_soup=make_request_using_cache(dig_url+nav_list[2]['href'])
+    sub_soup=BeautifulSoup(detailed_soup,'html.parser')
     sub_data=sub_soup.find_all('div',style='width: 860px; height: 32px; line-height: 32px; background: #f8f8f8;; padding: 0px 20px; color:#444; font-size: 9pt; border-bottom: 1px solid #eee;')
     stat_list=[]
     for item in sub_data:
@@ -96,20 +95,87 @@ def get_social(channel):
         data_tup=(clean[0],clean[2],clean[4],clean[5],clean[8],clean[9],clean[12])
         c_stat_list.append(data_tup)
     stat_list=c_stat_list
-    print(stat_list)
+    
+    #Scrapes for the projection data
+    detailed_soup=make_request_using_cache(dig_url+nav_list[1]['href'])
+    future_soup=BeautifulSoup(detailed_soup,'html.parser')
+    lv1_soup=future_soup.find('div',style='width: 900px; float: left;')
+    lv2_soup=lv1_soup.find_all('div',class_='TableMonthlyStats')
+    future_list=[]
+    for item in lv2_soup:
+        future_list.append(item.text.strip())
+
+    return(summary_list,stat_list,future_list)
+
+def init_db(db_name):
+    #code to create a new database goes here
+    #handle exception if connection fails by printing the error
+    try:
+        conn = sqlite3.connect(db_name)
+    except Error as e:
+        print(e)    
+
+    statement = '''
+    DROP TABLE IF EXISTS 'Tweets';
+    '''
+    cur=conn.cursor()
+    cur.execute(statement)
+    conn.commit()
+
+    create_cur=conn.cursor()
+    statement= '''
+    CREATE TABLE "Tweets"(
+    'TweetId' INTEGER PRIMARY KEY,
+    'TweetText' TEXT NOT NULL,
+    'RetweetCount' INTEGER NOT NULL,
+    'UserId' TEXT NOT NULL,
+    'ScreenName' TEXT NOT NULL,
+    'Location' TEXT NOT NULL,
+    'FollowerCount' INTEGER NOT NULL
+    );
+     '''
+    create_cur.execute(statement)
+    conn.commit()
+    conn.close()
+
+consumer_key=secrets.TWITTER_API_KEY
+consumer_secret=secrets.TWITTER_API_SECRET
+access_token=secrets.TWITTER_ACCESS_TOKEN
+access_secret=secrets.TWITTER_ACCESS_SECRET
+def get_tweets(search_term, num_tweets):
+    auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
+    auth.set_access_token(access_token, access_secret)
+    api = tweepy.API(auth)
+    searched_tweets = [status for status in tweepy.Cursor(api.search, q=search_term).items(num_tweets)]
+    return searched_tweets
+
+
+def insert_tweet_data(tweets):
+    #add code to connect to database and get a Cursor
+    conn=sqlite3.connect(db_name)
+    cur=conn.cursor()
+
+    #Add code to insert each of these data of interest to the Tweets table
+    for tweet in tweets:
+        if tweet.user.location=="":
+            empty="No Location"
+        else:
+            empty=tweet.user.location
+        insertion = (tweet.id, tweet.text.encode('utf8'), tweet.retweet_count, tweet.user.id,tweet.user.screen_name,empty,tweet.user.followers_count)
+        statement = 'INSERT INTO "Tweets" '
+        statement += 'VALUES (?, ?, ?, ?, ?, ?, ?)'
+        cur.execute(statement, insertion)
     
 
-#gets revenue over a period of time
-def get_timed_rev():
-    pass
-
+    #Close database connection
+    conn.commit()
+    conn.close()
 #gets tweets and gives sentiment analysis 
 def get_tweet_senti():
     pass
 
-#gets tweets for youtuber mentioned
-def get_tweets():
-    pass
 
-#get_channel_info('Philip Defranco')
-get_social('KSI')
+
+tweets=get_tweets('Phillip Defranco',30)
+init_db(db_name)
+insert_tweet_data(tweets)
